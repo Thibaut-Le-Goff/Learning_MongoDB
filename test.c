1a00ecabef3ace96e35b25edd2f6854408c37949
Thibaut Le Goff <thibaut.le.goff27@gmail.com>
2023-11-25 17:13:41 +0100

    Add files via upload

--- test.c
+++ test.c
@@ -5,17 +5,9 @@
 // gcc -o test test.c $(pkg-config --cflags --libs libmongoc-1.0) && ./test > test.txt
 
 int main(void) {
-    mongoc_client_t *client = NULL;
     bson_error_t error = {0};
-    mongoc_database_t *database = NULL;
-    bson_t *command = NULL;
     bson_t reply = BSON_INITIALIZER;
-    const bson_t *doc;
-    bson_t *query;
-    mongoc_collection_t *collection;
-    mongoc_cursor_t *cursor;
 
-    char *str;
     int return_state = 0;
     bool ok = true;
     int64_t count;
@@ -25,86 +17,91 @@ int main(void) {
     mongoc_init();
 
     //client = mongoc_client_new("mongodb://127.0.0.1:27017");
-    client = mongoc_client_new("mongodb://172.17.0.2:27017");
-
+    mongoc_client_t *client = mongoc_client_new("mongodb://172.17.0.2:27017");
     if (!client) {
         fprintf(stderr, "Failed to create a MongoDB client.\n");
 
         return_state = 1;
-        goto cleanup;
+        goto cleanup_client;
     }
 
     // Get a handle on the "admin" database.
-    //database = mongoc_client_get_database(client, "admin");
-    database = mongoc_client_get_database(client, "user");
+    mongoc_database_t *database = mongoc_client_get_database(client, "user");
     if (!database) {
         fprintf(stderr, "Failed to get a MongoDB database handle.\n");
         return_state = 1;
-        goto cleanup;
+        goto cleanup_database;
     }
 
-    collection = mongoc_client_get_collection(client, "new_york", "restaurants");
+    mongoc_collection_t *collection = mongoc_client_get_collection(client, "new_york", "restaurants");
 
-    query = bson_new();
-    //BSON_APPEND_UTF8(query, "name", "Harriet'S Kitchen");
-    //BSON_APPEND_UTF8(query, "grade", "A");
-    BSON_APPEND_UTF8(query, "cuisine", "American");
-    //BSON_APPEND_UTF8(query, "score", "12");
-    BSON_APPEND_UTF8(query, "borough", "Manhattan");
+    bson_t *query = bson_new();
+    BSON_APPEND_UTF8(query, "borough", "Brooklyn");
+    BSON_APPEND_UTF8(query, "cuisine", "Italian");
+    BSON_APPEND_UTF8(query, "address.street", "5 Avenue");
 
-    cursor = mongoc_collection_find_with_opts(collection, query, NULL, NULL);
+    //const bson_value_t *value;
+    //value = bson_iter_value("/pizza/i");
 
-    //doc = bson_new_from_json((const uint8_t *) "{\"name\": \"Harriet'S Kitchen\"}", -1, &error);
-    //doc = bson_new_from_json((const uint8_t *) "{\"score\": \"12\"}", -1, &error);
-    // "name" : "Harriet'S Kitchen",
+    //BSON_APPEND_VALUE(query, "name", value);
+    //BSON_APPEND_UTF8(query, "name", '/"pizza"/i');
+    //BSON_APPEND_VALUE(query, "name", '/"pizza"/i');
+    //BSON_APPEND_UTF8(query, "name", /"pizza"/i);
 
+    mongoc_cursor_t *cursor = mongoc_collection_find_with_opts(collection, query, NULL, NULL);
+
+    const bson_t *doc = NULL;
+    char *str;
     while (mongoc_cursor_next (cursor, &doc)) {
         str = bson_as_canonical_extended_json(doc, NULL);
-        printf ("%s\n", str);
-        bson_free (str);
+        printf("%s\n", str);
+        bson_free(str);
     }
-
-    //doc = bson_new_from_json((const uint8_t *) "{}", -1, &error);
-    //doc = bson_new_from_json((const uint8_t *) "{}", -1, &error);
-    //doc = bson_new_from_json((const uint8_t *) "{\"score\": \"12\"}", -1, &error);
-
-    /*
-    count = mongoc_collection_count_documents(collection, doc, NULL, NULL, NULL, NULL);
-
-    if (count < 0) {
-        fprintf(stderr, "%s\n", error.message);
-    } else {
-        printf("%" PRId64 "\n", count);
-    }
-    */
-
-    //printf("new_york : %s\n", collection);
-
-    // Ping the database.
-    command = BCON_NEW("ping", BCON_INT32(1));
-    ok = mongoc_database_command_simple(database, command, NULL, &reply, &error);
-
-    if (!ok) {
-        fprintf(stderr, "error: %s\n", error.message);
-        return_state = 1;
-        goto cleanup;
-    }
-    bson_destroy(&reply);
-
-    printf("Pinged your deployment. You successfully connected to MongoDB!\n");
 
     // Perform cleanup.
-    cleanup:
-        mongoc_client_destroy(client);
-        mongoc_database_destroy(database);
-        bson_destroy(command);
-        //bson_destroy(reply);
-        //bson_destroy(doc);
-        bson_destroy(query);
-        mongoc_collection_destroy(collection);
-        mongoc_cursor_destroy(cursor);
+    bson_free((char*)doc);
+    mongoc_collection_destroy(collection);
+    bson_destroy(query);
+    mongoc_cursor_destroy(cursor);
 
+    cleanup_database:
+        mongoc_database_destroy(database);
+
+    cleanup_client:
+        mongoc_client_destroy(client);
         mongoc_cleanup();
 
-        return return_state;
+    return return_state;
 }
+
+/*
+{
+    "address": {
+        "building": "19", 
+
+        "coord":{
+            "type":"Point", 
+            "coordinates" : [-73.9919172, 40.7224662]
+        }, 
+
+        "street": "Stanton Street", 
+        "zipcode": "10002"
+    }, 
+
+    "borough": "Manhattan", 
+    "cuisine": "American", 
+
+    "grades": [{
+
+        "date": {
+            "$date": 1403827200000
+        }, 
+
+        "grade": "A", 
+        "score": 11
+    }], 
+
+    "name": "Cocktail Bodega", 
+    "restaurant_id": "50010278"
+}
+*/
